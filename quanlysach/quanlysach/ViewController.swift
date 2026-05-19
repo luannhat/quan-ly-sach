68de23edd60ac43405bf17ad68ffc91664cb860e
luannhat44 <luannhat44@gmail.com>
2026-05-19 10:42:22 +0700

    initial project setup

---
+++ quanlysach/quanlysach/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quanlysach
+//
+//  Created by  User on 19.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ quanlysach/quanlysachUITests/quanlysachUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  quanlysachUITestsLaunchTests.swift
+//  quanlysachUITests
+//
+//  Created by  User on 19.05.2026.
+//
+
+import XCTest
+
+final class quanlysachUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
