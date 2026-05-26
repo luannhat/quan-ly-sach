6f7d7452f83df90cad02bc164ebf882a686427d5
luannhat44 <luannhat44@gmail.com>
2026-05-26 05:02:32 +0300

    commit file logincontroller,registercontroller, loginviewcontroller

---
+++ quanlysach/quanlysach/Login.swift
@@ -0,0 +1,29 @@
+//
+//  Login.swift
+//  quanlysach
+//
+//  Created by  User on 26.05.2026.
+//
+
+import UIKit
+
+class Login: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ quanlysach/quanlysach/RegisterViewController.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterViewController.swift
+//  quanlysach
+//
+//  Created by  User on 26.05.2026.
+//
+
+import Foundation
